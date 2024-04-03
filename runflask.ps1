ac553e8bcdcf27503127b0ae0261929855457ae5
Nikolaj <102536857+Nikolajkng@users.noreply.github.com>
2024-04-03 13:46:39 +0200

    added script for windows in powershell

--- runflask.ps1
+++ runflask.ps1
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "Setting Flask App Environment..."
-export FLASK_APP=GUI/main.py
+set FLASK_APP=GUI/main.py
 sleep 1
 
 echo "Setting Flask Environment to Developer Mode..."
-export FLASK_ENV=development
+set FLASK_ENV=development
 sleep 1
 
 echo "Running Flask..."
