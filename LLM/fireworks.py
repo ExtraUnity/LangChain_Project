e8925489c2c74acd10b4d250a7f218a7c621e84a
Marimusmaximus <107123960+Marimusmaximus@users.noreply.github.com>
2024-06-10 10:45:55 +0200

    Replaced custom math functions with build-in CustomCalculatorTool

--- LLM/fireworks.py
+++ LLM/fireworks.py
@@ -6,68 +6,115 @@ from langchain_community.utilities import OpenWeatherMapAPIWrapper
 from langchain_core.tools import tool
 from langchain_fireworks import ChatFireworks
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.agents import AgentExecutor
-from langchain.agents import AgentExecutor
-from langchain.agents import AgentExecutor
+
+from langchain.chains import LLMMathChain
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_openai_tools_agent
+from langchain.pydantic_v1 import BaseModel, Field
+from langchain.tools import BaseTool, StructuredTool, tool
+from typing import Optional, Type
+
 from langchain_core.messages import HumanMessage, AIMessage
 import numpy
 import asyncio
 from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
-import os
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_community.chat_models.huggingface import ChatHuggingFace
 
 
+math_llm = ChatOpenAI(
+    base_url="https://api.fireworks.ai/inference/v1",
+    api_key="4kGE92EQWNc7YvDDQqLoohUt0x8HdW8b3fjkq6ZQrs8FOEQk",
+    model="accounts/fireworks/models/mixtral-8x7b-instruct",
+    temperature=0.0,
+)
+
+class CalculatorInput(BaseModel):
+    query: str = Field(description="should be a mathematical expression")
+
+class CustomCalculatorTool(BaseTool):
+    name: str = "Calculator"
+    description: str = "Tool to evaluate mathemetical expressions"
+    args_schema: Type[BaseModel] = CalculatorInput
+
+    def _run(self, query: str) -> str:
+        """Use the tool."""
+        return LLMMathChain(llm=math_llm, verbose=True).run(query)
+
+    async def _arun(self, query: str) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("not support async")
+
+
+@tool
+def install_oceanwave3d():
+    """Builds a docker image with the OceanWave3D simulator"""
+    subprocess.run(["bash", "./install_oceanwave3d.sh"])
+
+@tool
+def run_oceanwave3d_simulation():
+    """Run a simulation with the OceanWave3D tool."""
+    subprocess.run(["bash", "./run_simulation.sh"])
+
+@tool
+def get_weather_info(city: str, country: str):
+    """Get the weather information"""
+    os.environ["OPENWEATHERMAP_API_KEY"] =  "a15039154ac226a73909c312586ea4c8"
+    weather = OpenWeatherMapAPIWrapper()
+    weather_data = weather.run(f"{city},{country}")
+    return weather_data
 ######################################################
 # Agent Tools
 ######################################################
-@tool
-def add(first_int: int, second_int: int) -> int:
-    """Adds two integers together."""
-    return first_int + second_int
+# @tool
+# def add(first_int: int, second_int: int) -> int:
+#     """Adds two integers together."""
+#     return first_int + second_int
 
-@tool
-def subtract(first_int: int, second_int: int) -> int:
-    """Subtracts two integers together."""
-    return first_int - second_int
+# @tool
+# def subtract(first_int: int, second_int: int) -> int:
+#     """Subtracts two integers together."""
+#     return first_int - second_int
 
-@tool
-def multiply(first_int: int, second_int: int) -> int:
-    """Multiply two integers together."""
-    return first_int * second_int
+# @tool
+# def multiply(first_int: int, second_int: int) -> int:
+#     """Multiply two integers together."""
+#     return first_int * second_int
 
-@tool
-def divide(first_int: int, second_int: int) -> int:
-    """Divides two integers together."""
-    return first_int // second_int
+# @tool
+# def divide(first_int: int, second_int: int) -> int:
+#     """Divides two integers together."""
+#     return first_int // second_int
 
-@tool
-def exponentiate(base: float, exponent: float) -> float:
-    """Exponentiate the base to the exponent power."""
-    return base**exponent
+# @tool
+# def exponentiate(base: float, exponent: float) -> float:
+#     """Exponentiate the base to the exponent power."""
+#     return base**exponent
 
-@tool
-def squareroot(integer: int) -> int:
-    """Takes the square root of an integer"""
-    return numpy.sqrt(integer)
+# @tool
+# def squareroot(integer: int) -> int:
+#     """Takes the square root of an integer"""
+#     return numpy.sqrt(integer)
 
-@tool
-def quadraticEquation(a:float, b:float, c:float):
-    """Solves a quadratic equation of form: ax²+bx+c = 0 with respect to x"""
-    if a != 0:
-        d = (b**2)-(4*a*c)
-        if d > 0:
-            x1 = ((-b) + numpy.sqrt(d))/(2*a)
-            x2 = ((-b) - numpy.sqrt(d))/(2*a)
-            return x1, x2
-        elif d == 0:
-            x = (-b)/2*a 
-            return x
-        else:
-            raise Exception("no solutions")
-    else:
-        raise Exception("a cannot be 0 in quadratic equation") 
+# @tool
+# def quadraticEquation(a:float, b:float, c:float):
+#     """Solves a quadratic equation of form: ax²+bx+c = 0 with respect to x"""
+#     if a != 0:
+#         d = (b**2)-(4*a*c)
+#         if d > 0:
+#             x1 = ((-b) + numpy.sqrt(d))/(2*a)
+#             x2 = ((-b) - numpy.sqrt(d))/(2*a)
+#             return x1, x2
+#         elif d == 0:
+#             x = (-b)/2*a 
+#             return x
+#         else:
+#             raise Exception("no solutions")
+#     else:
+#         raise Exception("a cannot be 0 in quadratic equation") 
 
 @tool
 def install_oceanwave3d():
@@ -169,7 +216,7 @@ def fireworks(user_input, APIKey):
 
 
     # Agent:
-    tools = [add, subtract, multiply, divide, exponentiate, squareroot, get_weather_info, quadraticEquation, run_oceanwave3d_simulation, install_oceanwave3d] 
+    tools = [CustomCalculatorTool(), get_weather_info, run_oceanwave3d_simulation, install_oceanwave3d] 
     prompt = hub.pull("hwchase17/structured-chat-agent")
     agent = create_structured_chat_agent(llm, tools, prompt)
     agent_executor = AgentExecutor(
